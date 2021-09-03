cf50507922a6f75e0b6405443e128088bfbd4c51
Faye <153318364@qq.com>
2021-09-03 13:42:26 +0800

    在文件改名的基础上加入了文件分配的功能

--- src/main/java/ExcelUtil.java
+++ src/main/java/ExcelUtil.java
@@ -1,6 +1,10 @@
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.ss.usermodel.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.List;
 
 /**
@@ -10,9 +14,7 @@ import java.util.List;
  */
 public class ExcelUtil {
 
-    public static void main(String[] args) throws IOException {
-        parseFromExcel("H:\\清源镇中理化实验操作考试学生成绩表.xlsx","H:\\清源镇中5.27理化实验录制视频");
-    }
+    //根据分数给文件夹改名
      public static void parseFromExcel(String excelPath,String filePath) throws IOException {
          FileInputStream fis = new FileInputStream(excelPath);
          Workbook workbook = WorkbookFactory.create(fis);
@@ -29,13 +31,13 @@ public class ExcelUtil {
                  String target = sb.append(number).toString();
                  sb.append("_" +grade);
                  String newName = sb.toString();
-                 fileReName(filePath,target,newName);
+                 fileRename(filePath,target,newName);
              }
          }
 
      }
 
-     public static void fileReName(String path,String target,String newName) throws FileNotFoundException {
+     public static void fileRename(String path,String target,String newName) throws FileNotFoundException {
          File file = new File(path);
          File[] files = file.listFiles();
          for(int i=0;i<files.length;i++){
@@ -47,27 +49,62 @@ public class ExcelUtil {
 
      }
 
-     public static void attributeFile(String chemistryPath,String physicsPath,String excelPath) throws IOException {
+     public static void attributeFile(String chemistryPath,String physicsPath,String excelPath,String srcPath) throws IOException {
+         Logger logger = LoggerFactory.getLogger("分配文件日志");
+
          FileInputStream fis = new FileInputStream(excelPath);
          Workbook workbook = WorkbookFactory.create(fis);
          Sheet sheet = workbook.getSheetAt(0);
          int lastRowNum = sheet.getLastRowNum();
+
          for(int i=1;i<lastRowNum;i++){
+             String fileName = null;
              Row row = sheet.getRow(i);
              String className = row.getCell(4).getStringCellValue();
-             String questionName = row.getCell(5).getStringCellValue();
+             int questionName = (int)row.getCell(5).getNumericCellValue();
              String targetDictionaryName  = className + questionName;
-             //文件的名字
-             String fileName = null;
-             if(row.getCell(3).getNumericCellValue() < 10){
-                 fileName = row.getCell(0).getNumericCellValue() + "_" + row.getCell(3).getNumericCellValue();
+             //缺考,无文件
+             if((int)row.getCell(3).getNumericCellValue()==-2){
+                 logger.warn((long)row.getCell(0).getNumericCellValue() + " " + "该学生缺考");
+                 continue;
+             }
+             else if(row.getCell(3).getNumericCellValue() < 10){
+                 fileName = (long)row.getCell(0).getNumericCellValue() + "_" + (int)row.getCell(3).getNumericCellValue();
              }
              else {
                  StringBuilder sb = new StringBuilder();
-                 int num = (int)row.getCell(0).getNumericCellValue();
+                 long num = (long)row.getCell(0).getNumericCellValue();
+                 String studentName = row.getCell(1).getStringCellValue();
                  fileName = sb.append(num).toString();
              }
+             //创建源文件夹和目标文件夹
+             File srcFile = new File(srcPath + "\\" + fileName);
+             File targetFile = null;
+             //如果源文件不存在,则直接跳过
+             if(!srcFile.exists()){
+                 logger.warn(srcFile.toString() + " " + "文件不存在!");
+                 continue;
+             }
+             //物理和化学文件名的判断
+             if(className.equals("物理")){
+                 targetFile = new File(physicsPath + "\\" +targetDictionaryName);
+             }
+             else if (className.equals("化学")) {
+                 targetFile = new File(chemistryPath + "\\" + targetDictionaryName);
+             }
+
+             FileUtils.copyDirectoryToDirectory(srcFile,targetFile);
+
+             logger.info(srcFile.toString() + " " +"传输完成");
+             if(i == lastRowNum-1){
+                 logger.info("全部文件传输完成");
+             }
+
+
          }
+
+
+
      }
 
 
